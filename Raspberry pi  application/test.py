76c87579ccd81d8b792f49b21c26afdf2a1629de
haitrieu123580 <haitrieu123580@gmail.com>
2023-04-03 20:25:51 +0700

    test upload file to firebase

--- Raspberry pi  application/test.py
+++ Raspberry pi  application/test.py
@@ -11,6 +11,7 @@ from scipy.signal import butter,lfilter
 from python_speech_features import mfcc
 from turn import AlphaBot
 import RPi.GPIO as GPIO 
+import pyrebase
 import time
 model_path ='/home/admin/PBL5_BabyCryingDetection_raspberry/Raspberry pi  application/model.tflite'
 with open(model_path,'rb') as f:
@@ -44,7 +45,21 @@ def feature(soundfile):
         logg.append(xx)
         
     return  logg  
-
+def upload(file_path):
+    config = {
+        'apiKey': "AIzaSyAM0MseLOCEPKQVKVF-O0ZoHVwu7X14ecg",
+        "authDomain": "test-app-a74f1.firebaseapp.com",
+        "projectId": "test-app-a74f1",
+        "storageBucket": "test-app-a74f1.appspot.com",
+        "messagingSenderId": "236232065512",
+        "appId": "1:236232065512:web:f824cf5ef653a013c5a45d",
+        "measurementId": "G-C7GVFRF735",
+        "serviceAccount":'Raspberry pi  application\serviceAccount.json',
+        "databaseURL":'https://test-app-a74f1-default-rtdb.firebaseio.com/'
+    }
+    firebase = pyrebase.initialize_app(config=config)
+    storage = firebase.storage()
+    storage.child('sound_test.wav').put(file_path)
 def doafter5():
     l = None
     livesound = None
@@ -89,6 +104,7 @@ def doafter5():
     Ab = AlphaBot()
     if soundclass==1:  
         Ab.forward()
+        upload('rec.wav')
     else:
         Ab.stop()
     os.remove('rec.wav')
